2f3a6dfa573bdd9504c391c75541909e2d488946
adamanteye <ada@adamanteye.cc>
2025-05-09 02:22:03 +0800

    feat: update sha256sum on pushing
    Signed-off-by: adamanteye <ada@adamanteye.cc>

--- push.sh
+++ push.sh
@@ -4,10 +4,13 @@ SOURCE_COLOR="\033[33m"
 RESET_COLOR="\033[0m"
 EXTRA_IDENT=2
 
-content=$1
+content=$(cat $1)
 source=$2
 
 content_length=$(echo -n "$content" | wc -m)
 spaces=$(printf "%$((content_length + EXTRA_IDENT))s" "")
 
-echo -e "$content\n$spaces$SOURCE_COLOR-- $source$RESET_COLOR\n%"
+echo -e "$content\n$spaces$SOURCE_COLOR-- $source$RESET_COLOR\n%" >>ada
+
+sum=$(sha256sum ada | cut -d ' ' -f 1)
+sed -i "s|sha256sums=('.*')|sha256sums=('$sum')|" PKGBUILD
